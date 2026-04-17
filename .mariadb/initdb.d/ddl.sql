57cd9b9f1050c0a9d4abd106567ee7597be9eecb
shellfolders <232290329+shellfolders@users.noreply.github.com>
2026-04-17 15:58:30 +0900

    데이터베이스 저장소 변경

---
+++ .mariadb/initdb.d/ddl.sql
@@ -0,0 +1,6 @@
+USE mysql;
+
+CREATE DATABASE `highgooh` /*!40100 COLLATE 'utf8mb4_uca1400_ai_ci' */
+CREATE DATABASE `skm` /*!40100 COLLATE 'utf8mb4_uca1400_ai_ci' */
+CREATE DATABASE `triplevalues` /*!40100 COLLATE 'utf8mb4_uca1400_ai_ci' */
+CREATE DATABASE `with` /*!40100 COLLATE 'utf8mb4_uca1400_ai_ci' */
