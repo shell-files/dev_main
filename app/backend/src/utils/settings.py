dfd28798add701e6486933e67d09bcfa6b529d97
NamYoungjun <wanyj2002@gmail.com>
2026-04-28 17:03:08 +0900

    redis 초안작성 v0.02

--- app/backend/src/utils/settings.py
+++ app/backend/src/utils/settings.py
@@ -2,6 +2,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
   host_ip: str
+  redis_host: str = "redis"
+  redis_port: int = 6379
+  redis_db: int = 0
 
   model_config = SettingsConfigDict(
     env_file=".env",
