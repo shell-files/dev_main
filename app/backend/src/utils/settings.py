d2c2cbd42be9085d51891c06f2e081f9fc4b48dc
sooah-0208 <santacoyaki11@gmail.com>
2026-04-28 16:50:33 +0900

    mariadb연결로직생성(#16)

--- app/backend/src/utils/settings.py
+++ app/backend/src/utils/settings.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-  # host_ip: str
+  host_ip: str
   # --------------------------
   # db.py
   # --------------------------
