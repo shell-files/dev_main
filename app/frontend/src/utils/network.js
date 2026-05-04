40826330bcf312eb5c3c1edba2fa8e711c44919a
x2bjvhmmwg-gif <as2237as@gmail.com>
2026-05-04 19:37:46 +0900

    API 연동 (#86 #77 #92)

--- app/frontend/src/utils/network.js
+++ app/frontend/src/utils/network.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  // withCredentials: true,
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
